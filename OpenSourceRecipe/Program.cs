0bab8d5f2f7b02815ace5f5924e80c19768d634a
Dan <57096365+danrfletcher@users.noreply.github.com>
2024-01-05 17:01:32 +0000

    Register controller

--- OpenSourceRecipe/Program.cs
+++ OpenSourceRecipe/Program.cs
@@ -147,6 +147,7 @@ builder.Services.AddAuthentication(options =>
 // Repositories
 builder.Services.AddScoped<UserRepository>();
 builder.Services.AddScoped<RecipeRepository>();
+builder.Services.AddScoped<IngredientRepository>();
 
 // Controllers
 builder.Services.AddControllers();
