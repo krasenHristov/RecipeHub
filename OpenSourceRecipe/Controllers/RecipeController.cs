5336cac7d12da99bc7d827171bbe14496d2c116d
Dan <57096365+danrfletcher@users.noreply.github.com>
2024-01-08 13:44:57 +0000

    Creates RecipeRating post & patch endpoints

--- OpenSourceRecipe/Controllers/RecipeController.cs
+++ OpenSourceRecipe/Controllers/RecipeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using OpenSourceRecipes.Models;
 using OpenSourceRecipes.Services;
@@ -74,6 +75,47 @@ public class RecipeController(RecipeRepository recipeRepository) : ControllerBas
         try
         {
             return Ok(await recipeRepository.GetForkedRecipes(userId, cuisineId, forkedFromId, originalRecipeId));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPost("api/recipes/rating")]
+    public async Task<ActionResult> PostRecipeRating(RecipeRatingDto ratingDetails)
+    {
+        try
+        {
+            int? userIdFromToken = int.Parse(User.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value!);
+
+            if (userIdFromToken != ratingDetails.UserId)
+            {
+                return Unauthorized();
+            }
+
+            var newRating = await recipeRepository.CreateRecipeRating(ratingDetails);
+            return CreatedAtAction(nameof(PostRecipeRating), newRating);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPatch("api/recipes/rating")]
+    public async Task<ActionResult> PatchRecipeRating(RecipeRatingDto ratingDetails)
+    {
+        try
+        {
+            int? userIdFromToken = int.Parse(User.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value!);
+
+            if (userIdFromToken != ratingDetails.UserId)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(await recipeRepository.UpdateRecipeRating(ratingDetails));
         }
         catch (Exception e)
         {
