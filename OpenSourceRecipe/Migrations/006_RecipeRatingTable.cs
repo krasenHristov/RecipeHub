068680b29d95597cb109cbea193706afc0fee090
Dan <57096365+danrfletcher@users.noreply.github.com>
2024-01-08 13:45:13 +0000

    Add migration for unique constraint in reciperatings table

--- OpenSourceRecipe/Migrations/006_RecipeRatingTable.cs
+++ OpenSourceRecipe/Migrations/006_RecipeRatingTable.cs
@@ -9,15 +9,31 @@ public class CreateRecipeRatingTable : Migration
     {
         Execute.Sql("CREATE TABLE \"RecipeRating\"" +
                     "(" +
-                    "\"RecipeId\" INT NOT NULL REFERENCES \"Recipe\" (\"RecipeId\")," +
-                    "\"UserId\" INT NOT NULL REFERENCES \"User\" (\"UserId\")," +
+                    "\"RecipeId\" INT NOT NULL REFERENCES \"Recipe\" (\"RecipeId\") ON DELETE CASCADE," +
+                    "\"UserId\" INT NOT NULL REFERENCES \"User\" (\"UserId\") ON DELETE CASCADE," +
                     "\"RATING\" INT NOT NULL" +
                     ");"
                     );
     }
+
 
     public override void Down()
     {
         Execute.Sql("DROP TABLE \"RecipeRating\"");
     }
 }
+
+[Migration(2024010106_01)]
+public class AddUniqueConstraintToRecipeRating : Migration
+{
+    public override void Up()
+    {
+        Execute.Sql("ALTER TABLE \"RecipeRating\" ADD CONSTRAINT \"UniqueRecipeUser\" UNIQUE (\"RecipeId\", \"UserId\");");
+    }
+
+    public override void Down()
+    {
+        Execute.Sql("ALTER TABLE \"RecipeRating\" DROP CONSTRAINT \"UniqueRecipeUser\";");
+    }
+}
+
